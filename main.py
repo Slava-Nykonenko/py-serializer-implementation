1e91f901edd8000b48c3362224613cb838722774
Viacheslav Nykonenko <v.nykonenko@gmail.com>
2025-10-01 21:23:15 +0100

    implemented CarSerializer (car/serializers.py), serialize_car_object and deserialize_car_object (main.py)

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
+from io import BytesIO
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+
 from car.models import Car
+from car.serializers import CarSerializer
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    serializer = CarSerializer(instance=car)
+    json = JSONRenderer().render(serializer.data)
+    return json
 
 
 def deserialize_car_object(json: bytes) -> Car:
-    pass
+    stream = BytesIO(json)
+    data = JSONParser().parse(stream)
+    serializer = CarSerializer(instance=data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return serializer.instance
